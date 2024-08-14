8223e1eaf0c60a8d2af5292df28c588cddd257c1
dhandy <dhandy.raju@swireshipping.com>
2024-08-14 19:24:54 +0530

    ngrx setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, importProvidersFrom } from '@angular/core';
+import { NgModule, importProvidersFrom, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HTTP_INTERCEPTORS, provideHttpClient, HttpClient, HttpClientModule} from '@angular/common/http';
 import { ApiInterceptor } from './interceptor/api-interceptor';
 import { authInterceptor } from './interceptor/auth.interceptor';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { authInterceptor } from './interceptor/auth.interceptor';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers:[provideHttpClient(),
     importProvidersFrom(HttpClientModule),
